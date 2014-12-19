970eaecec6211c4f4dcaad44bc2a475a231c939d
colt <majf.colt@gmail.com>
2014-12-19 08:48:42 +0800

    PagerDemo

---
+++ PagerViewDemo/src/main/java/com/ryg/fragment/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ryg.fragment;
+
+import java.util.List;
+
+import android.os.Bundle;
+
+import com.ryg.fragment.ui.IndicatorFragmentActivity;
+import com.ryg.indicatorfragment.R;
+
+public class MainActivity extends IndicatorFragmentActivity {
+
+    public static final int FRAGMENT_ONE = 0;
+    public static final int FRAGMENT_TWO = 1;
+    public static final int FRAGMENT_THREE = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int supplyTabs(List<TabInfo> tabs) {
+        tabs.add(new TabInfo(FRAGMENT_ONE, getString(R.string.fragment_one),
+                FragmentOne.class));
+        tabs.add(new TabInfo(FRAGMENT_TWO, getString(R.string.fragment_two),
+                FragmentTwo.class));
+        tabs.add(new TabInfo(FRAGMENT_THREE, getString(R.string.fragment_three),
+                FragmentThree.class));
+
+        return FRAGMENT_TWO;
+    }
+
+}
